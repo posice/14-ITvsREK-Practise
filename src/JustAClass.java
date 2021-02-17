1d4b8843d23404047204a98295bcc99e8b1e0d86
Jean-Pierre <jean.ambrosius@tu-dortmund.de>
2021-02-17 11:37:37 +0100

    Kleines Übungsprojekt mit 6 Methoden

---
+++ src/JustAClass.java
@@ -0,0 +1,78 @@
+/**
+ * Created by aos on 26.04.2017.
+ * Updated by aos on 17.02.2021.
+ */
+public class JustAClass {
+
+    //TODO: Arbeite die Methoden von oben nach unten ab. Teste immer über die Klasse MainProgramm, ob die Methode das gewünschte liefert.
+    /**
+     * Bestimmt eine zufällige ganze Zahl zwischen 1 und 6 (Grenzen einschließlich).
+     * @return
+     */
+    private int getRandom(){
+        int rand = (int)(Math.random()*6) + 1;
+        System.out.println("Random: " + rand);
+        return rand;
+    }
+
+
+    /**
+     * Summiert bis zur Zahl n einschließlich alle ganzen Zahlen auf und
+     * gibt die Summe zurück.
+     * @param n
+     * @return
+     */
+    public int sumUpIt(int n){
+        return 0;
+    }
+
+    /**
+     * Summiert bis zur Zahl n einschließlich alle ganzen Zahlen auf und
+     * gibt die Summe zurück.
+     * @param n
+     * @return
+     */
+    public int sumUpRek(int n){
+        return 0;
+    }
+
+    /**
+     * Bestimmt n zufällige Ergebnisse zwischen 1 und 6 (Grenzen einschließlich),
+     * summiert diese auf und gibt die Summe zurück.
+     * @param n
+     * @return
+     */
+    public int sumUpRandomIt(int n){
+        return 0;
+    }
+
+    /**
+     * Bestimmt n zufällige Ergebnisse zwischen 1 und 6 (Grenzen einschließlich),
+     * summiert diese auf und gibt die Summe zurück.
+     * @param n
+     * @return
+     */
+    public int sumUpRandomRek(int n){
+        return 0;
+    }
+
+    /**
+     * Bestimmt n zufällige positive, ganze Zahlen zwischen 1 und 6.
+     * Gibt die größte dieser zufälligen Zahlen zurück.
+     * @param n
+     * @return die größte zufällig Zahl, die im Verlauf der Methode erstellt wurde.
+     */
+    public int biggestOfRandomIt(int n){
+        return -1;
+    }
+
+    /**
+     * Bestimmt n zufällige positive, ganze Zahlen zwischen 1 und 6.
+     * Gibt die größte dieser zufälligen Zahlen zurück.
+     * @param n
+     * @return die größte zufällig Zahl, die im Verlauf der Methode erstellt wurde.
+     */
+    public int biggestOfRandomRek(int n){
+        return -1;
+    }
+}
---
+++ src/MainProgram.java
@@ -0,0 +1,13 @@
+/**
+ * Created by aos on 26.04.2017.
+ * Redefindes by aos on 17.02.2021.
+ */
+public class MainProgram {
+
+    public static void main(String[] args){
+        JustAClass jc = new JustAClass();
+
+        System.out.println("Summe der ersten 10 ganzen Zahlen-Iterativ: "+ jc.sumUpIt(10));
+        System.out.println("Summe der ersten 10 ganzen Zahlen-Rekursiv: "+ jc.sumUpRek(10));
+    }
+}
